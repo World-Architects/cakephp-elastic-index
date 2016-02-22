8f7ea789e1bad37dfb512de4d460caacf98a2b2e
Florian Krämer <florian.kraemer@cakedc.com>
2016-02-22 11:40:21 +0100

    PSR2 formatting... :(

--- src/Model/Behavior/ElasticIndexBehavior.php
+++ src/Model/Behavior/ElasticIndexBehavior.php
@@ -12,135 +12,142 @@ use Cake\Utility\Inflector;
 
 class ElasticIndexBehavior extends Behavior {
 
-	/**
-	 * Default config
-	 *
-	 * @param array
-	 */
-	protected $_defaultConfig = [
-		'type' => null,
-		'connection' => 'elastic',
-		'autoIndex' => true
-	];
+    /**
+     * Default config
+     *
+     * @param array
+     */
+    protected $_defaultConfig = [
+        'type' => null,
+        'connection' => 'elastic',
+        'autoIndex' => true
+    ];
 
-	/**
-	 * Elastic Search Type object
-	 *
-	 * @param \Cake\Elastic\Type
-	 */
-	protected $_elasticType = null;
+    /**
+     * Elastic Search Type object
+     *
+     * @param \Cake\Elastic\Type
+     */
+    protected $_elasticType = null;
 
-	/**
-	 * Constructor
-	 *
-	 * @param \Cake\ORM\Table
-	 * @param array $config
-	 */
-	public function __construct(Table $table, array $config = []) {
-		$this->_defaultConfig['type'] = Inflector::underscore($table->table());
-		parent::__construct($table, $config);
-		$this->elasticIndex($this->config('type'), $this->config('connection'));
-	}
+    /**
+     * Constructor
+     *
+     * @param \Cake\ORM\Table
+     * @param array $config
+     */
+    public function __construct(Table $table, array $config = [])
+    {
+        $this->_defaultConfig['type'] = Inflector::underscore($table->table());
+        parent::__construct($table, $config);
+        $this->elasticIndex($this->config('type'), $this->config('connection'));
+    }
 
-	/**
-	 * Gets and sets an index type to use for building the index.
-	 *
-	 * @param string $type
-	 * @param string $connection
-	 * @return \Cake\ElasticSearch\Type;
-	 */
-	public function elasticIndex($type = null, $connection = null) {
-		if (!empty($type)) {
-			if (empty($connection)) {
-				$connection = $this->config('connection');
-			}
-			$this->_elasticType = TypeRegistry::get($this->config('type'), [
-				'connection' => ConnectionManager::get($this->config('connection'))
-			]);
-		}
-		return $this->_elasticType;
-	}
+    /**
+     * Gets and sets an index type to use for building the index.
+     *
+     * @param string $type
+     * @param string $connection
+     * @return \Cake\ElasticSearch\Type;
+     */
+    public function elasticIndex($type = null, $connection = null)
+    {
+        if (!empty($type)) {
+            if (empty($connection)) {
+                $connection = $this->config('connection');
+            }
+            $this->_elasticType = TypeRegistry::get($this->config('type'), [
+                'connection' => ConnectionManager::get($this->config('connection'))
+            ]);
+        }
+        return $this->_elasticType;
+    }
 
-	/**
-	 * After save
-	 *
-	 * @param \Cake\Event\Event;
-	 * @param \Cake\Datasource\EntityInterface
-	 * @return void
-	 */
-	public function afterSave(Event $event, EntityInterface $entity) {
-		if ($this->config('autoIndex') === true) {
-			$this->saveIndexDocument($entity);
-		}
-	}
+    /**
+     * After save
+     *
+     * @param \Cake\Event\Event;
+     * @param \Cake\Datasource\EntityInterface
+     * @return void
+     */
+    public function afterSave(Event $event, EntityInterface $entity)
+    {
+        if ($this->config('autoIndex') === true) {
+            $this->saveIndexDocument($entity);
+        }
+    }
 
-	/**
-	 * Saves and updates a document in the index used by the table the behavior is attached to.
-	 *
-	 * @param \Cake\Datasource\EntityInterface
-	 * @return void
-	 */
-	public function saveIndexDocument(EntityInterface $entity) {
-		if (method_exists($this->_table, 'getIndexData')) {
-			$indexData = $this->_table->getIndexData($entity);
-		} else {
-			$indexData = $entity;
-		}
+    /**
+     * Saves and updates a document in the index used by the table the behavior is attached to.
+     *
+     * @param \Cake\Datasource\EntityInterface
+     * @return void
+     */
+    public function saveIndexDocument(EntityInterface $entity)
+    {
+        if (method_exists($this->_table, 'getIndexData')) {
+            $indexData = $this->_table->getIndexData($entity);
+        } else {
+            $indexData = $entity;
+        }
 
-		if ($indexData instanceof EntityInterface) {
-			$indexData = $indexData->toArray();
-		}
+        if ($indexData instanceof EntityInterface) {
+            $indexData = $indexData->toArray();
+        }
 
-		if ($entity->isNew()) {
-			$elasticEntity = $this->elasticIndex()->newEntity($indexData);
-		} else {
-			$elasticEntity = $this->_findElasticDocument($entity);
-			if (empty($elasticEntity)) {
-				$elasticEntity = $this->elasticIndex()->newEntity($indexData);
-			} else {
-				$elasticEntity = $this->elasticIndex()->patchEntity($elasticEntity, $indexData);
-			}
-		}
+        if ($entity->isNew()) {
+            $elasticEntity = $this->elasticIndex()->newEntity($indexData);
+        } else {
+            $elasticEntity = $this->_findElasticDocument($entity);
+            if (empty($elasticEntity)) {
+                $elasticEntity = $this->elasticIndex()->newEntity($indexData);
+            } else {
+                $elasticEntity = $this->elasticIndex()->patchEntity($elasticEntity, $indexData);
+            }
+        }
 
-		return $this->elasticIndex()->save($elasticEntity);
-	}
+        return $this->elasticIndex()->save($elasticEntity);
+    }
 
-	/**
-	 * After delete
-	 *
-	 * @param \Cake\Event\Event;
-	 * @param \Cake\Datasource\EntityInterface
-	 * @return void
-	 */
-	public function afterDelete(Event $event, EntityInterface $entity) {
-		if ($this->config('autoIndex') === true) {
-			$this->deleteIndexDocument($entity);
-		}
-	}
+    /**
+     * After delete
+     *
+     * @param \Cake\Event\Event;
+     * @param \Cake\Datasource\EntityInterface
+     * @return void
+     */
+    public function afterDelete(Event $event, EntityInterface $entity)
+    {
+        if ($this->config('autoIndex') === true) {
+            $this->deleteIndexDocument($entity);
+        }
+    }
 
-	/**
-	 * Deletes an index document.
-	 *
-	 * @param \Cake\Datasource\EntityInterface
-	 * @return void
-	 */
-	public function deleteIndexDocument(EntityInterface $entity) {
-		$elasticEntity = $this->_findElasticDocument($entity);
-		$this->elasticIndex()->delete($elasticEntity);
-	}
+    /**
+     * Deletes an index document.
+     *
+     * @param \Cake\Datasource\EntityInterface
+     * @return void
+     */
+    public function deleteIndexDocument(EntityInterface $entity)
+    {
+        $elasticEntity = $this->_findElasticDocument($entity);
+        $this->elasticIndex()->delete($elasticEntity);
+    }
 
-	/**
-	 * Finds an elastic index document for an entity of the current table.
-	 *
-	 * @param \Cake\ORM\EntityInterface
-	 * @return \Cake\ElasticSearch\Datasource\Document
-	 */
-	protected function _findElasticDocument($entity) {
-		return $this->elasticIndex()
-			->find()
-			->where([
-				'_id' => (string)$entity->{$this->_table->primaryKey()}
-			])
-			->first();
-	}
+    /**
+     * Finds an elastic index document for an entity of the current table.
+     *
+     * @param \Cake\ORM\EntityInterface
+     * @return \Cake\ElasticSearch\Datasource\Document
+     */
+    protected function _findElasticDocument($entity)
+    {
+        return $this->elasticIndex()
+            ->find()
+            ->where([
+                '_id' => (string)$entity->{$this->_table->primaryKey()}
+            ])
+            ->first();
+    }
 }
--- tests/TestCase/Model/Behavior/ElasticIndexBehaviorTest.php
+++ tests/TestCase/Model/Behavior/ElasticIndexBehaviorTest.php
@@ -6,62 +6,67 @@ use Cake\ORM\TableRegistry;
 
 class ElasticIndexBehavior extends TestCase {
 
-	/**
-	 * Fixtures
-	 *
-	 * @var array
-	 */
-	public $fixtures = [
-		'Projects' => 'plugin.psa/elastic_index.projects',
-		'Tasks' => 'plugin.psa/elastic_index.tasks',
-	];
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'Projects' => 'plugin.psa/elastic_index.projects',
+        'Tasks' => 'plugin.psa/elastic_index.tasks',
+    ];
 
-	/**cls
-	 * setUp method
-	 *
-	 * @return void
-	 */
-	public function setUp() {
-		parent::setUp();
-		$this->Projects = TableRegistry::get('Projects');
-		$this->Projects->hasMany('Tasks');
-	}
+    /**cls
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->Projects = TableRegistry::get('Projects');
+        $this->Projects->hasMany('Tasks');
+    }
 
-	/**
-	 * tearDown method
-	 *
-	 * @return void
-	 */
-	public function tearDown() {
-		unset($this->Tasks);
-		parent::tearDown();
-	}
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Tasks);
+        parent::tearDown();
+    }
 
-	/**
-	 * testSomething
-	 *
-	 * @return void
-	 */
-	public function testSomething() {
-		$this->Projects->addBehavior('Psa/ElasticIndex.ElasticIndex', [
-			'connection' => 'test_elastic'
-		]);
-		$entity = $this->Projects->newEntity([
-			'title' => 'Some Project',
-			'body' => 'Some content',
-			'tasks' => [
-				['title' => 'foo', 'body' => 'bar'],
-				['title' => 'foo2', 'body' => 'bar2'],
-			]
-		]);
-		$this->Projects->save($entity);
-	}
+    /**
+     * testSomething
+     *
+     * @return void
+     */
+    public function testSomething()
+    {
+        $this->Projects->addBehavior('Psa/ElasticIndex.ElasticIndex', [
+            'connection' => 'test_elastic'
+        ]);
+        $entity = $this->Projects->newEntity([
+            'title' => 'Some Project',
+            'body' => 'Some content',
+            'tasks' => [
+                ['title' => 'foo', 'body' => 'bar'],
+                ['title' => 'foo2', 'body' => 'bar2'],
+            ]
+        ]);
+        $this->Projects->save($entity);
+    }
 
-	public function testAfterSave() {
+    public function testAfterSave()
+    {
 
-	}
+    }
 
-	public function testAfterDelete() {
+    public function testAfterDelete()
+    {
 
-	}
+    }
 }
