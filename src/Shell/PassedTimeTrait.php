6a3878222d31206662c0be29173a614c5b290a45
Florian Krämer <florian.kraemer@kreative-design.net>
2017-05-23 09:34:22 +0200

    Changing the PassedTimeTrait to show only times needed

--- src/Shell/PassedTimeTrait.php
+++ src/Shell/PassedTimeTrait.php
@@ -1,8 +1,6 @@
 <?php
 namespace Psa\ElasticIndex\Shell;
 
-use Cake\Chronos\Chronos;
-
 trait PassedTimeTrait {
 
     /**
@@ -31,17 +29,17 @@ trait PassedTimeTrait {
     protected function _calcTime($duration)
     {
         return [
-            'days' => floor( $duration / (3600 * 24)),
-            'hours' => floor( ($duration / 3600 ) % 24),
-            'minutes' => floor( ( $duration / 60 ) % 60 ),
-            'seconds' => ( $duration % 60 )
+            'days' => floor($duration / (3600 * 24)),
+            'hours' => floor(($duration / 3600) % 24),
+            'minutes' => floor(($duration / 60) % 60),
+            'seconds' => ($duration % 60)
         ];
     }
 
     /**
      * Prints the passed time since _startStartTime()
      *
-     * @return  void
+     * @return void
      */
     protected function _showPassedTime()
     {
@@ -53,6 +51,13 @@ trait PassedTimeTrait {
             __n('{0} Minute', '{0} Minutes', $time['minutes'], [$time['minutes']]),
             __n('{0} Second', '{0} Seconds', $time['seconds'], [$time['seconds']])
         ];
+
+        foreach ($time as $key => $value) {
+            if ($value === 0) {
+                unset($output[$key]);
+            }
+            break;
+        }
 
         $this->out(implode(', ', $output));
     }
